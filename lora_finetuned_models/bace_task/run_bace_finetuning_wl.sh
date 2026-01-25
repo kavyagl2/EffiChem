22a4eb4b849492995173e0162439fc99509a84af
raghvendra5688 <raghvendra5688@gmail.com>
2026-01-25 07:57:10 +0000

    Make the changes for BACE dataset

---
+++ lora_finetuned_models/bace_task/run_bace_finetuning_wl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH -J bace_ft_wl
+#SBATCH -o out_bace_ft_wl.txt
+#SBATCH -e out_bace_ft_wl.err
+#SBATCH -p gpu-A100
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80000
+#SBATCH -A A100
+#SBATCH -q a100_qos
+
+module load cuda12.6/toolkit/12.6.2
+python --version
+nvidia-smi
+nvcc --version
+
+export MAMBA_EXE='/export/home/rmall/.local/bin/micromamba';
+export MAMBA_ROOT_PREFIX='/export/home/rmall/micromamba';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell bash --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
+
+micromamba env list
+micromamba activate effichem
+
+python3 finetune_bace_wl.py
